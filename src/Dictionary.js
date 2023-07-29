6ade1a4798a29574b39825e7050fcc314300e235
CelineCambier <cambier.celine2@gmail.com>
2023-07-28 18:10:48 -0700

    Added dictionary api integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,21 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictonary() {
   let [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data.meanings[0]);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword} definition`);
+
+    let apiKey = "e48e1aab0acf4fc215c13f02ct8bo5ab";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
