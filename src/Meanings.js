27276e6ad3b7dde83ce3bfacf8294c9589ca5b50
CelineCambier <cambier.celine2@gmail.com>
2023-07-29 22:43:07 -0700

    Rendered synonyms

--- src/Meanings.js
+++ src/Meanings.js
@@ -1,12 +1,16 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meanings(props) {
   return (
     <div className="Meanings">
       <h3>{props.meanings.partOfSpeech}</h3>
       <div>
-        <div className="definition">{props.meanings.definition}</div>
-        <div className="example">{props.meanings.example}</div>
+        <div className="definition"><strong>Definitions: </strong>{props.meanings.definition}</div>
+        <br/>
+        <div className="example"><strong>Example: </strong>{props.meanings.example}</div>
+        <br/>
+        <Synonyms synonyms={props.meanings.synonyms}/>
       </div>
     </div>
   );
