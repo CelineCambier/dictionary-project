479629be18688fe495efb641324035df534d6e74
CelineCambier <cambier.celine2@gmail.com>
2023-07-29 21:43:44 -0700

    Rendered the word meanings

--- src/Meanings.js
+++ src/Meanings.js
@@ -1,6 +1,13 @@
 import React from "react";
 
 export default function Meanings(props) {
-    console.log(props.meanings);
-    return ("Hello from meanings");
+  return (
+    <div className="Meanings">
+      <h3>{props.meanings.partOfSpeech}</h3>
+      <div>
+        <div className="definition">{props.meanings.definition}</div>
+        <div className="example">{props.meanings.example}</div>
+      </div>
+    </div>
+  );
 }
