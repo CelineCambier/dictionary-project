1a592259d6af64ee6ecd6b3a0295c2f054b6678a
CelineCambier <cambier.celine2@gmail.com>
2023-08-06 12:33:10 -0700

    Added photos api

--- src/App.js
+++ src/App.js
@@ -8,8 +8,9 @@ export default function App() {
       <div className="container">
         <header className="App-header">
           <img src={logo} className="App-logo img-fluid" alt="logo" />
+          <h1>Dictionary App</h1>
         </header>
-        <Dictionary defaultKeyword="wine"/>
+        <Dictionary defaultKeyword="wine" />
         <footer className="App-footer">
           <small>Coded by Celine Cambier</small>
         </footer>
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,22 +1,32 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictonary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState({});
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
-  function handleResponse(response) {
+  function handleDictionaryResponse(response) {
     setResults(response.data);
+  }
+
+  function handlePhotoResponse(response) {
+    setPhotos(response.data.photos);
   }
 
   function search() {
     let apiKey = "e48e1aab0acf4fc215c13f02ct8bo5ab";
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
 
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+    let photoApiKey = "e48e1aab0acf4fc215c13f02ct8bo5ab";
+    let photoApiUrl = `https://api.shecodes.io/images/v1/search?query=${keyword}&key=${photoApiKey}`;
+    axios.get(photoApiUrl).then(handlePhotoResponse);
   }
 
   function handleSubmit(event) {
@@ -37,15 +47,20 @@ export default function Dictonary(props) {
     return (
       <div className="Dictionary">
         <section>
-          <h1>What word do you want to look up?</h1>
+          <h2>What word do you want to look up?</h2>
           <form onSubmit={handleSubmit}>
-            <input type="search" onChange={handleKeywordChange} defaultValue={props.defaultKeyword}/>
+            <input
+              type="search"
+              onChange={handleKeywordChange}
+              defaultValue={props.defaultKeyword}
+            />
           </form>
           <div className="hint">
             Suggested words: sunset, wine, love, bed...
           </div>
         </section>
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
